abd659d4b7172b1579fd19a38601ca44f35fac16
Sergsniperrr <153460753+Sergsniperrr@users.noreply.github.com>
2025-03-28 17:02:50 +0300

    Initial commit

---
+++ Assets/Scripts/Bus/Pointer.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    [field: SerializeField] public Transform Target { get; private set; }
+}
---
+++ Assets/Scripts/Input/MouseInputHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MouseInputHandler : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+
+    private const int LeftMouseButton = 0;
+
+    private Ray _ray;
+    private RaycastHit _hitInfo;
+    private Collider _target;
+
+    private void Update()
+    {
+        _target = OnMouseClick();
+
+        if (_target == null)
+            return;
+
+        if (_target.TryGetComponent(out BusMover bus))
+            bus.Run();
+    }
+
+    private Collider OnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(LeftMouseButton))
+        {
+            _ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hitInfo))
+                return _hitInfo.collider;
+        }
+
+        return null;
+    }
+}
